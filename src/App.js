f11376c3d94dbb3630aa4930a913d39123884e0c
Xenia <yeyvra@gmail.com>
2023-01-21 19:40:49 +0600

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './POSTY.css';
+import nft from "./123.webp"
+
+function App() {
+  return (
+    <div class="App">
+        <header>
+        <div className="navbar">
+            <div className="logo">
+            POSTY
+            </div>
+            <div className='nav'>My posts</div>
+            <div className='nav nav2' >New!</div>
+            <div className='nav'>Friends</div>
+        </div>
+        </header>
+        <div className="main">
+
+            <div className="profile">
+                <img src=''/>
+                <p>0 folowers</p>
+                <p>Your profile</p>
+                <p>Username12345</p>
+                <p>Willy, whilty, good person</p>
+                <br/>
+
+            </div>
+
+        </div>
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
